fc9f68b7acef04ec76ccf66165ee567b1a5255c0
Eric Anderson <humanapp@users.noreply.github.com>
2026-03-25 20:13:51 -0500

    feat: improve install scripts

---
+++ packages/scripts/install.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const { execSync } = require("node:child_process");
+
+const packages = [
+    "core",
+    "ui",
+    "docs",
+    "typescript",
+    "ts-authoring",
+    "ts-protocol",
+];
+
+for (const pkg of packages) {
+    console.log(`Installing ${pkg}...`);
+    execSync(`npm --prefix ${pkg} install`, { stdio: "inherit" });
+}
