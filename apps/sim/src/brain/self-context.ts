c24c94d55f8765a0f6f8b149f447cd337143b11a
Eric Anderson <humanapp@users.noreply.github.com>
2026-04-11 19:27:35 -0700

    feat: add getTargetActor and getTargetPosition methods to SelfContext

--- apps/sim/src/brain/self-context.ts
+++ apps/sim/src/brain/self-context.ts
@@ -6,16 +6,21 @@ import {
   type MapValue,
   type MindcraftModuleApi,
   mkCallDef,
+  mkNativeStructValue,
+  NIL_VALUE,
   type StructValue,
   type Value,
   Vector2,
   VOID_VALUE,
 } from "@mindcraft-lang/core/app";
-import { getSelf } from "@/brain/execution-context-types";
+import { getSelf, getTargetActor } from "@/brain/execution-context-types";
 import { extractVector2, mkVector2Value, SimTypeIds } from "@/brain/type-system";
 
 export function registerSelfContext(api: MindcraftModuleApi) {
   const { types, functions } = api.brainServices;
+
+  const nullableActorRefTypeId = types.addNullableType(SimTypeIds.ActorRef);
+  const nullableVector2TypeId = types.addNullableType(SimTypeIds.Vector2);
 
   types.addStructMethods(
     ContextTypeIds.SelfContext,
@@ -29,6 +34,16 @@ export function registerSelfContext(api: MindcraftModuleApi) {
         name: "setPosition",
         params: List.from([{ name: "pos", typeId: SimTypeIds.Vector2 }]),
         returnTypeId: CoreTypeIds.Void,
+      },
+      {
+        name: "getTargetActor",
+        params: List.empty(),
+        returnTypeId: nullableActorRefTypeId,
+      },
+      {
+        name: "getTargetPosition",
+        params: List.empty(),
+        returnTypeId: nullableVector2TypeId,
       },
     ])
   );
@@ -60,6 +75,32 @@ export function registerSelfContext(api: MindcraftModuleApi) {
         if (!vec) return VOID_VALUE;
         self.sprite.setPosition(vec.X, vec.Y);
         return VOID_VALUE;
+      },
+    },
+    emptyCallDef
+  );
+
+  functions.register(
+    "SelfContext.getTargetActor",
+    false,
+    {
+      exec: (ctx: ExecutionContext): Value => {
+        const actor = getTargetActor(ctx);
+        if (!actor) return NIL_VALUE;
+        return mkNativeStructValue(SimTypeIds.ActorRef, actor);
+      },
+    },
+    emptyCallDef
+  );
+
+  functions.register(
+    "SelfContext.getTargetPosition",
+    false,
+    {
+      exec: (ctx: ExecutionContext): Value => {
+        const actor = getTargetActor(ctx);
+        if (!actor) return NIL_VALUE;
+        return mkVector2Value(new Vector2(actor.sprite.x, actor.sprite.y));
       },
     },
     emptyCallDef
